2731b3a584bfac54d148e1b199d0efb31c714f7b
Gerard Dethier <gerard@logion.network>
2021-06-14 09:18:06 +0200

    chore: fix build.

--- src/wallet-user/Wallet.test.tsx
+++ src/wallet-user/Wallet.test.tsx
@@ -1,91 +1,16 @@
-jest.mock('../logion-chain');
-jest.mock('./UserContext');
+jest.mock("../logion-chain");
 
-import React from 'react';
-import Wallet from './Wallet';
+import React from "react";
+import { mockAccount, shallowRender } from "../tests";
 import { setContextMock } from '../logion-chain';
-import { shallowRender, mockAccount } from '../tests';
+import Wallet from "./Wallet";
 
-test('Given disconnected, when rendering, then show disconnected and connect button', () => {
+test("renders", () => {
     setContextMock({
-        apiState: 'DISCONNECTED',
         injectedAccounts: [
             mockAccount("address", "name")
         ]
-    });
-
-    const tree = shallowRender(<Wallet />);
-
-    expect(tree).toMatchSnapshot();
-});
-
-test('Given connected, when rendering, then show account, no connect button and tokenization', () => {
-    setContextMock({
-        apiState: 'READY',
-        injectedAccounts: [
-            mockAccount("address", "Account name")
-        ],
-        connectedNodeMetadata: {
-            name: "Node name",
-            peerId: "Node peer ID"
-        }
-    });
-
-    const tree = shallowRender(<Wallet />);
-
-    expect(tree).toMatchSnapshot();
-});
-
-test('Given connected and no metadata, when rendering, then show account, no connect button, no node info, tokenization', () => {
-    setContextMock({
-        apiState: 'READY',
-        injectedAccounts: [
-            mockAccount("address", "Account name")
-        ],
-        connectedNodeMetadata: null
-    });
-
-    const tree = shallowRender(<Wallet />);
-
-    expect(tree).toMatchSnapshot();
-});
-
-test('Given connection error, when rendering, then show account, no button, no node info', () => {
-    setContextMock({
-        apiState: 'ERROR',
-        injectedAccounts: [
-            mockAccount("address", "Account name")
-        ],
-        connectedNodeMetadata: null
-    });
-
-    const tree = shallowRender(<Wallet />);
-
-    expect(tree).toMatchSnapshot();
-});
-
-test('Given connecting, when rendering, then show account, no button, no node info', () => {
-    setContextMock({
-        apiState: 'CONNECTING',
-        injectedAccounts: [
-            mockAccount("address", "Account name")
-        ],
-        connectedNodeMetadata: null
-    });
-
-    const tree = shallowRender(<Wallet />);
-
-    expect(tree).toMatchSnapshot();
-});
-
-test('Given no injected account, when rendering, then nothing displayed', () => {
-    setContextMock({
-        apiState: 'READY',
-        injectedAccounts: null,
-        connectedNodeMetadata: null
-    });
-
-    const tree = shallowRender(<Wallet />);
-
+    })
+    const tree = shallowRender(<Wallet/>)
     expect(tree).toMatchSnapshot();
 });
--- src/wallet-user/Wallet.tsx
+++ src/wallet-user/Wallet.tsx
@@ -1,56 +1,21 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 
-import { useLogionChain, ApiState, NodeMetadata } from '../logion-chain';
+import { useLogionChain } from '../logion-chain';
 
-import Shell from '../Shell';
-import UserRouter from "./UserRouter";
-import { UserContextProvider, useUserContext } from "./UserContext";
+import { UserContextProvider } from "./UserContext";
 import { DEFAULT_LEGAL_OFFICER } from "../legal-officer/Types";
-
-function status(apiState: ApiState, metadata: NodeMetadata | null): string {
-    if(apiState === 'READY' && metadata === null) {
-        return "connected";
-    } else if(apiState === 'READY' && metadata !== null) {
-        return `connected to node "${metadata.name}" (Peer ID: ${metadata.peerId})`;
-    } else if(apiState === 'DISCONNECTED') {
-        return "disconnected";
-    } else if(apiState === 'ERROR') {
-        return "disconnected (error)";
-    } else {
-        return "connecting";
-    }
-}
+import ContextualizedWallet from './ContextualizedWallet';
 
 export default function Wallet() {
-    const { injectedAccounts, apiState, connect, connectedNodeMetadata } = useLogionChain();
+    const { injectedAccounts } = useLogionChain();
 
     if(injectedAccounts === null) {
         return null;
     }
 
-    const connectButton = apiState === 'DISCONNECTED' ? <Button onClick={connect}>Connect</Button> : null;
-    const userContext = apiState === 'READY' ? <UserRouter /> : null;
-
     return (
         <UserContextProvider legalOfficerAddress={DEFAULT_LEGAL_OFFICER} userAddress={injectedAccounts[0].address}>
-            {
-                <Shell>
-                    <h1>You are ready to use the Logion wallet, congratulations!</h1>
-        
-                    <p>The following account(s) were detected:</p>
-                    <ul>
-                        {injectedAccounts.map(injectedAccount =>
-                            <li key={injectedAccount.address}>
-                                <Button variant="link" onClick={ () => useUserContext().setUserAddress!(injectedAccount.address) }>{injectedAccount.address} ({injectedAccount.meta.name || ""})</Button>
-                            </li>
-                        )}
-                    </ul>
-                    <p>You are currently {status(apiState, connectedNodeMetadata)}</p>
-                    {connectButton}
-                    {userContext}
-                </Shell>
-            }
+            <ContextualizedWallet />
         </UserContextProvider>
     );
 }
