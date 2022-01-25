a7045b8fd03a7536d25404df170f5f3648eed0e4
Gérard Dethier <gerard@logion.network>
2022-01-25 11:54:05 +0100

    chore: increase proxy TO for node.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-/usr/docker/wait-for-it.sh $NODE_HOST_PORT
+/usr/docker/wait-for-it.sh -t 30 $NODE_HOST_PORT
 /usr/docker/wait-for-it.sh $BACKEND_HOST_PORT
 
 # Run the main container command.
