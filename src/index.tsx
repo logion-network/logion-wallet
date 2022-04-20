39b218475a879bb230300bec5d3164a916a0bafe
Gérard Dethier <gerard@logion.network>
2022-04-20 11:58:21 +0200

    chore: fix app following React upgrade.
    This is a temporary workaround, see logion-network/logion-internal#429

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,7 @@ moment.locale('en-gb');
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
