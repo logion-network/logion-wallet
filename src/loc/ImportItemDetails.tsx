b0533ba2c687b0b8f7f4515efab06a7c53a2c4db
Gérard Dethier <gerard@logion.network>
2022-07-06 15:31:25 +0200

    feat: enable item file upload.
    logion-network/logion-internal#528

--- src/loc/ImportItemDetails.tsx
+++ src/loc/ImportItemDetails.tsx
@@ -7,12 +7,25 @@ export interface Item extends CollectionItem {
     submitted: boolean;
     failed: boolean;
     success: boolean;
+    upload: boolean;
 }
 
 export default function ImportItemDetails(props: { item: Item }) {
     return (
         <div className="ImportItemDetails">
             <pre>{ props.item.description }</pre>
+            {
+                props.item.files.length > 0 &&
+                <div className="item-file">
+                    <p>Attached file:</p>
+                    <ul>
+                        <li>Name: { props.item.files[0].name }</li>
+                        <li>Content type: { props.item.files[0].contentType }</li>
+                        <li>Hash: { props.item.files[0].hash }</li>
+                        <li>Size: { props.item.files[0].size.toString() } bytes</li>
+                    </ul>
+                </div>
+            }
         </div>
     );
 }
