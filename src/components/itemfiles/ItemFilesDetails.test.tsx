141007bf6b7e1d53b1bf00f75e30a54a85b22690
Gérard Dethier <gerard@logion.network>
2022-08-29 13:31:48 +0200

    feat: handle ownership check result.
    logion-network/logion-internal#562

--- src/components/itemfiles/ItemFilesDetails.test.tsx
+++ src/components/itemfiles/ItemFilesDetails.test.tsx
@@ -26,7 +26,8 @@ describe("ItemFilesDetails", () => {
             deliveries={ [{
                 copyHash: "0x72685ddcbec5052f9db2523252407990c24bb94d43b478d1a22411e612f3b650",
                 owner: "0x900edc98db53508e6742723988b872dd08cd09c2",
-                generatedOn: "2022-08-25T07:27:46.128Z"
+                generatedOn: "2022-08-25T07:27:46.128Z",
+                belongsToCurrentOwner: true,
             }] }
         />);
         expect(result).toMatchSnapshot();
