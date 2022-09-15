240ead1f784141cfb8b30067aa979eb38c48f5d1
Benoit Devos <benoit@logion.network>
2022-09-15 23:05:46 +0200

    feat: improve transaction type.

---
+++ src/common/TransactionType.test.tsx
@@ -0,0 +1,24 @@
+import { shallowRender } from "../tests";
+import TransactionType from "./TransactionType";
+import { Transaction } from "@logion/client/dist/TransactionClient";
+
+describe("TransactionType", () => {
+
+    it("has the right type for a logion pallet/method", () => {
+        const transaction = {
+            pallet: "logionLoc",
+            method: "addCollectionItem"
+        } as Transaction;
+        const result = shallowRender(<TransactionType transaction={ transaction } walletType="Wallet" address="" />);
+        expect(result).toMatchSnapshot();
+    })
+
+    it("generates a default type pallet/method", () => {
+        const transaction = {
+            pallet: "unknownPallet",
+            method: "unknownMethod"
+        } as Transaction;
+        const result = shallowRender(<TransactionType transaction={ transaction } walletType="Wallet" address="" />);
+        expect(result).toMatchSnapshot();
+    })
+})
