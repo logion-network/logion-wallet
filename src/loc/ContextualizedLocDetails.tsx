f0fd921c77cf0c9542abc39c4487b0639790c09d
Benoit Devos <benoit@logion.network>
2022-09-15 21:20:16 +0200

    chore: add title to identity LOC personal info section.
    logion-network/logion-internal#578

--- src/loc/ContextualizedLocDetails.tsx
+++ src/loc/ContextualizedLocDetails.tsx
@@ -52,39 +52,42 @@ function PersonalInfo(props: PersonalInfoProps) {
     const userPostalAddress = locRequest.userPostalAddress;
     const padding = "/";
     return (
-        <Row className="PersonalInfo">
-            <Col md={ 6 }>
-                <Detail label="Account Address" value={ locRequest.requesterAddress || padding } />
-                <Row>
-                    <Col style={ { flexGrow: 1 } }>
-                        <Detail label="First Name" value={ userIdentity?.firstName || padding } />
-                        <Detail label="Email" value={ userIdentity?.email || padding } />
-                    </Col>
-                    <Col style={ { flexGrow: 1 } }>
-                        <Detail label="Last Name" value={ userIdentity?.lastName || padding } />
-                        <Detail label="Phone Number" value={ userIdentity?.phoneNumber || padding } />
-                    </Col>
-                </Row>
-            </Col>
-            <Col md={ 6 }>
-                <Detail label="Line1" value={ userPostalAddress?.line1 || padding } />
-                <Detail label="Line2" value={ userPostalAddress?.line2 || padding } />
+        <div className="PersonalInfo">
+            <p className="title">Identity related record(s)</p>
+            <Row>
+                <Col md={ 6 }>
+                    <Detail label="Account Address" value={ locRequest.requesterAddress || padding } />
+                    <Row>
+                        <Col style={ { flexGrow: 1 } }>
+                            <Detail label="First Name" value={ userIdentity?.firstName || padding } />
+                            <Detail label="Email" value={ userIdentity?.email || padding } />
+                        </Col>
+                        <Col style={ { flexGrow: 1 } }>
+                            <Detail label="Last Name" value={ userIdentity?.lastName || padding } />
+                            <Detail label="Phone Number" value={ userIdentity?.phoneNumber || padding } />
+                        </Col>
+                    </Row>
+                </Col>
+                <Col md={ 6 }>
+                    <Detail label="Line1" value={ userPostalAddress?.line1 || padding } />
+                    <Detail label="Line2" value={ userPostalAddress?.line2 || padding } />
 
-                <Row>
-                    <Col style={ { flexGrow: 1 } }>
-                        <Detail label="Postal Code" value={ userPostalAddress?.postalCode || padding } />
-                    </Col>
-                    <Col style={ { flexGrow: 2 } }>
-                        <Detail label="City" value={ userPostalAddress?.city || padding } />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col style={ { flexGrow: 1 } }>
-                        <Detail label="Country" value={ userPostalAddress?.country || padding } />
-                    </Col>
-                </Row>
-            </Col>
-        </Row>
+                    <Row>
+                        <Col style={ { flexGrow: 1 } }>
+                            <Detail label="Postal Code" value={ userPostalAddress?.postalCode || padding } />
+                        </Col>
+                        <Col style={ { flexGrow: 2 } }>
+                            <Detail label="City" value={ userPostalAddress?.city || padding } />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col style={ { flexGrow: 1 } }>
+                            <Detail label="Country" value={ userPostalAddress?.country || padding } />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </div>
     )
 }
 
