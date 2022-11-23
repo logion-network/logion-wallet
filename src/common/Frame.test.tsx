735dbb72e7c3dbac3ebb300f6d727ee2bdb61146
Benoit Devos <benoit@logion.network>
2022-11-23 10:32:37 +0100

    chore: display Frame's title area only when needed.
    logion-network/logion-internal#685

--- src/common/Frame.test.tsx
+++ src/common/Frame.test.tsx
@@ -1,11 +1,39 @@
 import { shallowRender } from '../tests';
 
 import Frame from './Frame';
+import { Children } from "./types/Helpers";
 
-test("renders", () => {
-    const result = shallowRender(
-        <Frame>
-        </Frame>
-    );
-    expect(result).toMatchSnapshot();
-});
+describe("Frame", () => {
+
+    const children: Children = <p>Some children</p>;
+
+    it("renders", () => {
+        const result = shallowRender(
+            <Frame>
+                { children }
+            </Frame>
+        );
+        expect(result).toMatchSnapshot();
+    });
+
+    it("renders with title", () => {
+        const result = shallowRender(
+            <Frame title="Some title">
+                { children }
+            </Frame>
+        );
+        expect(result).toMatchSnapshot();
+    });
+
+
+    it("renders with title and icon", () => {
+        const result = shallowRender(
+            <Frame title="Some title" titleIcon={ { icon: { id: "vtp-icon" } } }>
+                { children }
+            </Frame>
+        );
+        expect(result).toMatchSnapshot();
+    });
+
+})
+
