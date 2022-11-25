f72e4b8ff032a97830ba750153b3e4e939a26411
Benoit Devos <benoit@logion.network>
2022-11-25 12:22:56 +0100

    chore: add tests.
    logion-network/logion-internal#688

---
+++ src/common/SubmitterName.test.tsx
@@ -0,0 +1,43 @@
+import { shallowRender } from "../tests";
+import SubmitterName from "./SubmitterName";
+import { DEFAULT_LEGAL_OFFICER, DEFAULT_IDENTITY } from "./TestData";
+import { LocData } from "@logion/client";
+import { LocItem } from "../loc/types";
+
+jest.mock('../logion-chain');
+
+describe("SubmitterName", () => {
+
+    const REQUESTER = "5H17oaxYsRpV18tYjMMbozN84JAVgG6MH5pNWjisGYDtc9WR";
+    const VTP = "5DtGNqi7yp5TqRcPckYVZh5XqdaSKE9rsDur5xur4tkmBAhB";
+    const loc = {
+        ownerAddress: DEFAULT_LEGAL_OFFICER,
+        requesterAddress: REQUESTER,
+        userIdentity: DEFAULT_IDENTITY,
+        selectedParties: [ {
+            address: VTP,
+            firstName: "Scott",
+            lastName: "Tiger",
+        } ]
+    } as LocData;
+
+    it("renders when submitter is Legal Officer", () => {
+
+        const locItem = { submitter: DEFAULT_LEGAL_OFFICER } as LocItem;
+        const tree = shallowRender(<SubmitterName loc={ loc } locItem={ locItem } />);
+        expect(tree).toMatchSnapshot()
+    })
+
+    it("renders when submitter is Requester", () => {
+        const locItem = { submitter: REQUESTER } as LocItem;
+        const tree = shallowRender(<SubmitterName loc={ loc } locItem={ locItem } />);
+        expect(tree).toMatchSnapshot()
+    })
+
+    it("renders when submitter is VTP", () => {
+        const locItem = { submitter: VTP } as LocItem;
+        const tree = shallowRender(<SubmitterName loc={ loc } locItem={ locItem } />);
+        expect(tree).toMatchSnapshot()
+    })
+
+})
--- src/legal-officer/client.test.ts
+++ src/legal-officer/client.test.ts
@@ -1,7 +1,7 @@
-import { EditableRequest, LogionClient } from "@logion/client";
+import { EditableRequest, LogionClient, OpenLoc, VerifiedThirdParty } from "@logion/client";
 import { UUID } from "@logion/node-api";
 import { AxiosInstance } from "axios";
-import { addLink } from "./client";
+import { addLink, getVerifiedThirdPartySelections } from "./client";
 
 describe("Legal Officer client", () => {
 
@@ -41,5 +41,55 @@ describe("Legal Officer client", () => {
                 target: target.toString(),
             })
         )
+    })
+
+    it("gets vtp selections ordered by last name", async () => {
+
+        function verifiedThirdParty(index: number): VerifiedThirdParty {
+            return {
+                address: "addr" + index,
+                identityLocId: "id" + index,
+                firstName: "Scott" + index,
+                lastName: "Tiger" + index,
+            }
+        }
+
+        const REQUESTER = verifiedThirdParty(1);
+        const NOT_SELECTED = verifiedThirdParty(2);
+        const SELECTED = verifiedThirdParty(3);
+        const FORMERLY_SELECTED = verifiedThirdParty(4);
+
+        const axios = {
+            get: jest.fn().mockResolvedValue({ data: { verifiedThirdParties: [ FORMERLY_SELECTED, REQUESTER, SELECTED, NOT_SELECTED ] } }),
+        } as unknown as AxiosInstance;
+
+        const client = {
+            buildAxios: () => axios,
+            legalOfficers: [],
+        } as unknown as LogionClient;
+
+        const locId = new UUID("0e16421a-2550-4be5-a6a8-1ab2239b7dc4");
+        const locState = {
+            data: () => ({
+                id: locId,
+                requesterAddress: REQUESTER.address,
+                selectedParties: [
+                    { ...SELECTED, selected: true },
+                    { ...FORMERLY_SELECTED, selected: false }
+                ],
+            }),
+            locsState: () => ({
+                client,
+            }),
+        } as unknown as OpenLoc;
+
+        const verifiedThirdParties: VerifiedThirdParty[] = await getVerifiedThirdPartySelections({ locState });
+
+        expect(axios.get).toBeCalledWith("/api/verified-third-parties");
+
+        expect(verifiedThirdParties.length).toEqual(3);
+        expect(verifiedThirdParties[0]).toEqual({ ...NOT_SELECTED, selected: false });
+        expect(verifiedThirdParties[1]).toEqual({ ...SELECTED, selected: true });
+        expect(verifiedThirdParties[2]).toEqual({ ...FORMERLY_SELECTED, selected: false });
     })
 });
