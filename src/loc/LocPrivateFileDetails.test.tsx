490ff46db1600863f3978b9e4c1aa297d921467c
Benoit Devos <benoit@logion.network>
2023-03-24 11:31:14 +0100

    feat: add size in file published data box.
    logion-network/logion-internal#822

--- src/loc/LocPrivateFileDetails.test.tsx
+++ src/loc/LocPrivateFileDetails.test.tsx
@@ -5,19 +5,31 @@ import { LocItem } from "./LocItem";
 
 describe("LocPrivateFileDetails", () => {
 
+    const item: LocItem = {
+        name: "a file",
+        value: "0xfb45e95061306e90fd154272ba3b4d67bb6d295feeccdc3a34572995f08e268a",
+        timestamp: null,
+        type: "Document",
+        submitter: TEST_WALLET_USER,
+        status: "DRAFT",
+        nature: "File's nature",
+        newItem: false,
+        template: false,
+    };
+
     it("renders", () => {
-        const item: LocItem = {
-            name: "a file",
-            value: "0xfb45e95061306e90fd154272ba3b4d67bb6d295feeccdc3a34572995f08e268a",
-            timestamp: null,
-            type: "Document",
-            submitter: TEST_WALLET_USER,
-            status: "DRAFT",
-            nature: "File's nature",
-            newItem: false,
-            template: false,
-        };
-        const element = shallowRender(<LocPrivateFileDetails item={ item } documentClaimHistory=""/>);
+        const element = shallowRender(<LocPrivateFileDetails
+            item={ { ...item, size: 42n } }
+            documentClaimHistory=""
+        />);
+        expect(element).toMatchSnapshot();
+    });
+
+    it("renders 0-sized file", () => {
+        const element = shallowRender(<LocPrivateFileDetails
+            item={ { ...item, size: 0n } }
+            documentClaimHistory=""
+        />);
         expect(element).toMatchSnapshot();
     });
 });
