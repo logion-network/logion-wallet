b0c50751ba283219f5ba30946462c854dddc1030
Gérard Dethier <gerard@logion.network>
2023-08-10 15:22:54 +0200

    chore: improve value fees display.
    logion-network/logion-internal#956

--- src/components/identity/CollectionInfo.tsx
+++ src/components/identity/CollectionInfo.tsx
@@ -1,10 +1,13 @@
 import { LocData } from "@logion/client";
+import { Currency } from "@logion/node-api";
 
 import { Row } from "../../common/Grid";
 import { Col } from "react-bootstrap";
 import Detail from "../../common/Detail";
 
 import "./CollectionInfo.css";
+import AmountFormat from "src/common/AmountFormat";
+import { toPrefixedLgnt } from "src/common/AmountCell";
 
 export interface Props {
     collection: LocData;
@@ -16,7 +19,10 @@ export function CollectionInfo(props: Props) {
             <p className="title">Collection parameters</p>
             <Row>
                 <Col>
-                    <Detail label="Value fee" value={ props.collection.valueFee?.toString() || "0" } />
+                    <Detail label="Value fee" value={ <span>
+                        <AmountFormat amount={
+                            props.collection.valueFee ? toPrefixedLgnt(props.collection.valueFee.toString()) : Currency.nLgnt(0n)
+                        }/> { Currency.SYMBOL }</span> } />
                 </Col>
             </Row>
         </div>
