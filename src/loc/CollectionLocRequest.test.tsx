22cca58c0eb90afc497e9169eeb6db31acdd4a82
Benoit Devos <benoit@logion.network>
2024-03-27 13:16:56 +0100

    feat: add option to request a new identity LOC.
    logion-network/logion-internal#1179

---
+++ src/loc/CollectionLocRequest.test.tsx
@@ -0,0 +1,107 @@
+import { UUID } from "@logion/node-api";
+import { DraftRequest, LocsState } from "@logion/client";
+import { twoLegalOfficers } from "../common/TestData";
+import { setLocsState, setMutateLocsState } from "../wallet-user/__mocks__/UserContextMock";
+import { render, waitFor, screen, getByText } from "@testing-library/react";
+import { clickByName, typeByLabel } from "../tests";
+import { navigate } from "../__mocks__/ReactRouterMock";
+import CollectionLocRequest from "./CollectionLocRequest";
+import userEvent from "@testing-library/user-event";
+import { LegalOfficerClass } from "@logion/client/dist/Types";
+
+jest.mock('../wallet-user/UserContext');
+jest.mock('../logion-chain');
+jest.mock('../common/CommonContext');
+
+const locId = new UUID("a2b9dfa7-cbde-414b-8cda-cdd221a57643");
+
+describe("CollectionLocRequest", () => {
+
+    it("should redirect when form is correctly filled in and submitted", async  () => {
+
+        setupLocsState(twoLegalOfficers);
+        render(<CollectionLocRequest backPath="back" />);
+
+        await selectLegalOfficer();
+        await fillInForm();
+        await clickByName("Submit");
+
+        await waitFor(() => expect(navigate).toBeCalledWith(`/user/loc/collection/${locId.toString()}`));
+    });
+
+    it("should disable form submission when no valid identity locs", async  () => {
+
+        setupLocsState([]);
+        render(<CollectionLocRequest backPath="back" />);
+        await waitFor(() => {
+            expect(screen.getByRole("button", { name: "Submit" })).toBeDisabled();
+            expect(screen.getByRole("button", { name: "Request an Identity Protection" })).toBeEnabled();
+        });
+    });
+
+    it("should disable form submission when no legal officer selected", async  () => {
+
+        setupLocsState(twoLegalOfficers);
+        render(<CollectionLocRequest backPath="back" />);
+        await waitFor(() => {
+            expect(screen.getByRole("button", { name: "Submit" })).toBeDisabled();
+            expect(screen.getByRole("button", { name: "Request an Identity Protection" })).toBeEnabled();
+        });
+    });
+
+    it("should disable form submission when no collection limit selected", async  () => {
+
+        setupLocsState(twoLegalOfficers);
+        render(<CollectionLocRequest backPath="back" />);
+        await selectLegalOfficer();
+        await waitFor(() => {
+            expect(screen.getByRole("button", { name: "Submit" })).toBeDisabled();
+            expect(screen.getByRole("button", { name: "Request an Identity Protection" })).toBeEnabled();
+        });
+    });
+
+})
+
+function setupLocsState(legalOfficersWithValidIdentityLoc: LegalOfficerClass[]) {
+    const draftRequest = {
+        locId,
+        locsState: () => locsState,
+        data: () => ({
+            id: locId,
+        }),
+    } as DraftRequest;
+    const locsState = {
+        legalOfficersWithValidIdentityLoc,
+        requestCollectionLoc: () => Promise.resolve(draftRequest),
+    } as unknown as LocsState;
+    setLocsState(locsState);
+    setMutateLocsState(async (mutator: (current: LocsState) => Promise<LocsState>): Promise<void> => {
+        await mutator(locsState);
+        return Promise.resolve();
+    });
+}
+
+async function selectLegalOfficer() {
+    const legalOfficer1Select = screen.getByTestId('legalOfficer1Group');
+    await userEvent.click(getByText(legalOfficer1Select, "Select..."));
+    await waitFor(() => userEvent.click(getByText(legalOfficer1Select, "Patrick Gielen (workload: 1)")));
+}
+
+async function fillInForm() {
+    await typeByLabel("Description", "description");
+    await typeByLabel("Value fee", "10");
+    await typeByLabel("Collection item fee", "10");
+    await typeByLabel("Tokens record fee", "10");
+    await selectAndEnterText(1, "Data number limit", "999");
+}
+
+async function selectAndEnterText(index: number, name: string, value: string) {
+    await selectCheckbox(index);
+    await typeByLabel(name, value);
+}
+
+async function selectCheckbox(index: number) {
+    const checkBoxes = screen.getAllByRole('checkbox');
+    await userEvent.click(checkBoxes[index]);
+}
+
--- src/loc/CollectionLocRequest.tsx
+++ src/loc/CollectionLocRequest.tsx
@@ -3,11 +3,14 @@ import Frame from "../common/Frame";
 import SelectLegalOfficer from "../wallet-user/trust-protection/SelectLegalOfficer";
 import { Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { LegalOfficerClass } from "@logion/client";
 import { useUserContext } from "../wallet-user/UserContext";
 import { useCommonContext } from "../common/CommonContext";
 import CollectionLocRequestForm from "./CollectionLocRequestForm";
+import IdentityLocCreation from "../wallet-user/IdentityLocCreation";
+import "./CollectionLocRequest.css";
+import ButtonGroup from "../common/ButtonGroup";
 
 export interface Props {
     backPath: string,
@@ -18,6 +21,13 @@ export default function CollectionLocRequest(props: Props) {
     const [ legalOfficer, setLegalOfficer ] = useState<LegalOfficerClass | null>(null);
     const { locsState } = useUserContext();
     const navigate = useNavigate();
+    const legalOfficersWithValidIdentityLoc = useMemo(() => {
+        if (locsState !== undefined) {
+            return locsState.legalOfficersWithValidIdentityLoc
+        } else {
+            return []
+        }
+    }, [ locsState ])
 
     return (
         <FullWidthPane
@@ -28,27 +38,52 @@ export default function CollectionLocRequest(props: Props) {
         >
             <Row>
                 <Col md={ 6 }>
-                    <Frame>
-                        <SelectLegalOfficer
-                            legalOfficer={ legalOfficer }
-                            legalOfficerNumber={ 1 }
-                            legalOfficers={ locsState?.legalOfficersWithValidIdentityLoc || [] }
-                            mode="select"
-                            otherLegalOfficer={ null }
-                            setLegalOfficer={ setLegalOfficer }
-                            label="Select your Legal Officer"
-                            fillEmptyOfficerDetails={ true }
-                            feedback="Required"
-                        />
-                    </Frame>
-                </Col>
-                <Col>
+                    { legalOfficersWithValidIdentityLoc.length > 0 &&
+                        <Frame>
+                            <SelectLegalOfficer
+                                legalOfficer={ legalOfficer }
+                                legalOfficerNumber={ 1 }
+                                legalOfficers={ legalOfficersWithValidIdentityLoc }
+                                mode="select"
+                                otherLegalOfficer={ null }
+                                setLegalOfficer={ setLegalOfficer }
+                                label="Select your Legal Officer"
+                                fillEmptyOfficerDetails={ true }
+                                feedback="Required"
+                            />
+                        </Frame>
+                    }
+                    { legalOfficersWithValidIdentityLoc.length > 0 &&
+                        <Frame className="request-additional-id-loc-frame">
+                            <p className="info-text">If you do not see the Logion Legal officer you are looking for,
+                                please request an Identity LOC to the Logion Legal Officer of your choice by
+                                clicking on the related button below:</p>
+                            <ButtonGroup>
+                                <IdentityLocCreation />
+                            </ButtonGroup>
+                        </Frame>
+                    }
+                    { legalOfficersWithValidIdentityLoc.length === 0 &&
+                        <Frame className="request-id-loc-frame">
+                            <p className="info-text">To submit a Collection LOC request, you must select a Logion Legal
+                                Officer who already executed an Identity LOC linked to your Polkadot address.</p>
+                            <p className="info-text">Please request an Identity LOC to the Logion Legal Officer of your
+                                choice:</p>
+                            <ButtonGroup>
+                                <IdentityLocCreation />
+                            </ButtonGroup>
+                        </Frame>
+                    }
+            </Col>
+            <Col>
+                <Frame disabled={ legalOfficer === null }>
                     <CollectionLocRequestForm
-                        colors={ colorTheme.dialog }
+                        colors={ colorTheme.frame }
                         legalOfficer={ legalOfficer?.address }
                     />
-                </Col>
-            </Row>
-        </FullWidthPane>
-    )
+                </Frame>
+            </Col>
+        </Row>
+</FullWidthPane>
+)
 }
