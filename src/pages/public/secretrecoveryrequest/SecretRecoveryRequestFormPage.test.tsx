423fe6b8d4534c15d4b637b5d9a2295186722c37
Gérard Dethier <gerard@logion.network>
2024-05-16 11:29:59 +0200

    feat: add secret recovery request form.
    logion-network/logion-internal#1257

---
+++ src/pages/public/secretrecoveryrequest/SecretRecoveryRequestFormPage.test.tsx
@@ -0,0 +1,118 @@
+import { LocRequestStatus, LogionClient, PublicLoc } from '@logion/client';
+import { UUID, LocType } from "@logion/node-api";
+import { screen, render, waitFor } from '@testing-library/react';
+import SecretRecoveryRequestFormPage from './SecretRecoveryRequestFormPage';
+import { clickByName, typeByLabel } from 'src/tests';
+import { setClientMock } from 'src/logion-chain/__mocks__/LogionChainMock';
+
+jest.mock("src/logion-chain");
+
+describe("SecretRecoveryRequestFormPage", () => {
+
+    it("shows success message", async () => {
+        setClientMock(mockClient(LOC_ID, {
+            locType: "Identity",
+            status: "CLOSED",
+        }));
+        render(<SecretRecoveryRequestFormPage />);
+        await fillInSecretRecoveryRequestForm();
+        await fillInIdentityForm();
+
+        await clickByName("Submit");
+
+        await waitFor(() => expect(screen.queryByText("Submission successful")).toBeInTheDocument());
+        expect(screen.queryByText(LOC_ID.toDecimalString())).toBeInTheDocument();
+        expect(screen.queryByText(SECRET_NAME)).toBeInTheDocument();
+        expect(screen.queryByText(CHALLENGE)).toBeInTheDocument();
+        expect(screen.queryByRole("button", { name: "Submit" })).not.toBeInTheDocument();
+    });
+
+    it("prevents submission if missing data", async () => {
+        render(<SecretRecoveryRequestFormPage />);
+        await clickByName("Submit");
+        await waitFor(() => expect(screen.queryByText("Some information above is not valid")).toBeInTheDocument());
+    });
+
+    it("prevents submission if LOC not found", async () => {
+        setClientMock(mockClient(new UUID(), {
+            locType: "Identity",
+            status: "CLOSED",
+        }));
+        render(<SecretRecoveryRequestFormPage />);
+        await fillInSecretRecoveryRequestForm();
+        await fillInIdentityForm();
+
+        await clickByName("Submit");
+
+        await waitFor(() => expect(screen.queryByText("Some information above is not valid")).toBeInTheDocument());
+        expect(screen.queryByText("LOC not found")).toBeInTheDocument();
+    });
+
+    it("prevents submission if not identity LOC", async () => {
+        setClientMock(mockClient(LOC_ID, {
+            locType: "Collection",
+            status: "CLOSED",
+        }));
+        render(<SecretRecoveryRequestFormPage />);
+        await fillInSecretRecoveryRequestForm();
+        await fillInIdentityForm();
+
+        await clickByName("Submit");
+
+        await waitFor(() => expect(screen.queryByText("Some information above is not valid")).toBeInTheDocument());
+        expect(screen.queryByText("LOC is not an Identity LOC")).toBeInTheDocument();
+    });
+
+    it("prevents submission if identity LOC not closed", async () => {
+        setClientMock(mockClient(LOC_ID, {
+            locType: "Identity",
+            status: "OPEN",
+        }));
+        render(<SecretRecoveryRequestFormPage />);
+        await fillInSecretRecoveryRequestForm();
+        await fillInIdentityForm();
+
+        await clickByName("Submit");
+
+        await waitFor(() => expect(screen.queryByText("Some information above is not valid")).toBeInTheDocument());
+        expect(screen.queryByText("LOC is not closed")).toBeInTheDocument();
+    });
+});
+
+function mockClient(expectedLocId: UUID, data: { locType: LocType, status: LocRequestStatus }) {
+    return {
+        public: {
+            findLocById: (args: { locId: UUID }) => {
+                if(args.locId.equalTo(expectedLocId)) {
+                    return Promise.resolve({
+                        data,
+                    } as PublicLoc);
+                } else {
+                    return Promise.resolve(undefined);
+                }
+            }
+        }
+    } as unknown as LogionClient;
+}
+
+const LOC_ID = new UUID();
+const SECRET_NAME = "Key";
+const CHALLENGE = "Some random challenge";
+
+async function fillInSecretRecoveryRequestForm() {
+    await typeByLabel("Identity LOC ID", LOC_ID.toDecimalString());
+    await typeByLabel("Secret name", SECRET_NAME);
+    await typeByLabel("Challenge", CHALLENGE);
+}
+
+async function fillInIdentityForm() {
+    await typeByLabel("First Name", "John");
+    await typeByLabel("Last Name", "Doe");
+    await typeByLabel("Email", "john@logion.network");
+    await typeByLabel("Phone Number", "+1234");
+
+    await typeByLabel("Line1", "?");
+    await typeByLabel("Postal Code", "?");
+    await typeByLabel("City", "?");
+    await typeByLabel("Country", "?");
+}
---
+++ src/pages/public/secretrecoveryrequest/SecretRecoveryRequestFormPage.tsx
@@ -0,0 +1,119 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useCommonContext } from "src/common/CommonContext";
+import Frame from "src/common/Frame";
+import IdentityForm, { FormValues as IdentityFormValues } from "src/components/identity/IdentityForm";
+
+import "./SecretRecoveryRequestFormPage.css";
+import SecretRecoveryRequestForm, { FormValues as SecretFormValues } from "./SecretRecoveryRequestForm";
+import ButtonGroup from "src/common/ButtonGroup";
+import Button from "src/common/Button";
+import { useCallback, useState } from "react";
+import Alert from "src/common/Alert";
+import Icon from "src/common/Icon";
+import CopyPasteButton from "src/common/CopyPasteButton";
+
+export default function SecretRecoveryRequestFormPage() {
+    const { colorTheme } = useCommonContext();
+    const { control: secretControl, trigger: secretTrigger, formState: { errors: secretErrors }, setValue, getValues } = useForm<SecretFormValues>();
+    const { control: identityControl, trigger: identityTrigger, formState: { errors: identityErrors } } = useForm<IdentityFormValues>();
+    const [ error, setError ] = useState<string>();
+    const [ submitting, setSubmitting ] = useState(false);
+    const [ submitted, setSubmitted ] = useState(false);
+
+    const submit = useCallback(async () => {
+        setError(undefined);
+        setSubmitting(true);
+        setSubmitted(false);
+        const results = await Promise.all([ secretTrigger(), identityTrigger() ]);
+        const formError = !(results[0] && results[1]);
+        if(formError) {
+            setError("Some information above is not valid");
+        } else {
+            try {
+                // TODO: actually submit data
+                setSubmitted(true);
+            } catch(e) {
+                if(e instanceof Error) {
+                    setError(e.message);
+                } else {
+                    setError("" + e);
+                }
+            }
+        }
+        setSubmitting(false);
+    }, [ secretTrigger, identityTrigger ]);
+
+    return (
+        <div className="SecretRecoveryRequestFormPage">
+            {
+                !submitted &&
+                <Container>
+                    <Row>
+                        <Col>
+                            <Frame
+                                title="Secret to recover"
+                            >
+                                <p>The information below can be found in an e-mail you received after
+                                    creating the recoverable secret.</p>
+                                <SecretRecoveryRequestForm
+                                    control={ secretControl }
+                                    errors={ secretErrors }
+                                    colors={ colorTheme.frame }
+                                    setValue={ setValue }
+                                />
+                            </Frame>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Frame
+                                title="Personal information"
+                            >
+                                <p>The information below must match what you provided in the Identity LOC
+                                    mentioned in the above frame.</p>
+                                <IdentityForm
+                                    control={ identityControl }
+                                    errors={ identityErrors }
+                                    colors={ colorTheme.frame }
+                                />
+                            </Frame>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <ButtonGroup>
+                            <Button onClick={ submit } disabled={ submitting }>Submit</Button>
+                        </ButtonGroup>
+                        {
+                            error !== undefined &&
+                            <div className="alert-container">
+                                <Alert variant="danger">{ error }</Alert>
+                            </div>
+                        }
+                    </Row>
+                </Container>
+            }
+            {
+                submitted &&
+                <Container>
+                    <Row>
+                        <Col>
+                            <Frame
+                                title="Submission successful"
+                                className="success-frame"
+                            >
+                                <p className="success-icon"><Icon icon={{ id: "ok" }} height="64"/></p>
+                                <p>Your request to recover secret <strong>{ getValues().secretName }</strong>{" "}
+                                    from LOC <strong>{ getValues().locId }</strong>{" "}
+                                    has been submitted successfully.</p>
+                                <p><strong>Please keep below challenge in a safe place, you will need it in order
+                                    to retrieve the secret once your request has been approved</strong>:</p>
+                                <p className="challenge">{ getValues().challenge } <CopyPasteButton value={ getValues().challenge } /></p>
+                            </Frame>
+                        </Col>
+                    </Row>
+                </Container>
+            }
+        </div>
+    );
+}
